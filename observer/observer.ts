2849ebeb88784d96d4f51e5aa161a3a9cd1226a0
Xavier Decuyper <hi@savjee.be>
2017-04-11 16:18:51 +0200

    Added Observer example

---
+++ observer/observer.ts
@@ -0,0 +1,81 @@
+// Temperature monitor (subject)
+// -> Temperature display
+// -> Thermostat
+// -> Fan
+// -> ...
+
+interface Subject{
+    registerObserver(o: Observer);
+    removeObserver(o: Observer);
+    notifyObservers();
+}
+
+interface Observer{
+    update(temperature: number);
+}
+
+// ------------------------------------------------------
+class WeatherStation implements Subject {
+    private observers: Observer[] = [];
+    private temperature: number;
+
+    registerObserver(o: Observer) {
+        this.observers.push(o);
+    }
+
+    removeObserver(o: Observer) {
+        let index = this.observers.indexOf(o);
+        this.observers.splice(index, 1);
+    }
+
+    notifyObservers() {
+        for (let observer of this.observers) {
+            observer.update(this.temperature);
+        }
+    }
+
+    setTemperature(temp: number) {
+        console.log('WeatherStation: new temperature measurement: ' + temp);
+        this.temperature = temp;
+        this.notifyObservers();
+    }
+}
+
+// ------------------------------------------------------
+class TemperatureDisplay implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        console.log('TemperatureDisplay: I need to update my display');
+    }
+}
+
+class Fan implements Observer {
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    update(temperature: number) {
+        if (temperature > 25) {
+            console.log('Fan: Its hot here, turning myself on...');
+        } else {
+            console.log('Fan: Its nice and cool, turning myself off...');
+        }
+    }
+}
+
+let weatherStation = new WeatherStation();
+
+let tempDisplay = new TemperatureDisplay(weatherStation);
+let fan = new Fan(weatherStation);
+
+weatherStation.setTemperature(20);
+weatherStation.setTemperature(30);
